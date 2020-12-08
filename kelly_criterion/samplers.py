ce074f25547c7f34595fb524d314b62be3b331f1
mgh35 <markgeorgehiggins@hotmail.com>
2020-12-07 20:09:56 -0500

    Add univariate Bernoulli

--- kelly_criterion/samplers.py
+++ kelly_criterion/samplers.py
@@ -1,8 +1,15 @@
 from scipy.stats import bernoulli
+from scipy.stats import uniform
 
 
 def make_bernoulli_return_sampler(p):
     def sampler(size):
         return 2 * bernoulli.rvs(p, size=size)-1
+
+    return sampler
+
+def make_uniform_return_sampler(upper, *, lower=-1):
+    def sampler(size):
+        return uniform.rvs(loc=lower, scale=upper-lower, size=size)
 
     return sampler
